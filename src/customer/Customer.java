adc4d7707d41ebb0c2f1073c24f6d208864ec676
Hadi <hadi@10.91.82.31>
2017-05-09 12:51:18 +0200

    Getters and setters

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -4,5 +4,23 @@ public class Customer {
 	
 	
 	private String name;
+	private float credit;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public float getCredit() {
+		return credit;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCredit(float credit) {
+		this.credit = credit;
+	}
+	
+	
 
 }
